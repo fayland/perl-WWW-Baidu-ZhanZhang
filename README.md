77379a42cbcb81f0296d7d673f2aa4e81f203f5a
Fayland Lam <fayland@gmail.com>
2016-08-31 14:19:22 +0800

    from milla new

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# perl-WWW-Baidu-ZhanZhang
-Baidu ZhanZhang push links
+# NAME
+
+WWW::Baidu::ZhanZhang - Blah blah blah
+
+# SYNOPSIS
+
+    use WWW::Baidu::ZhanZhang;
+
+# DESCRIPTION
+
+WWW::Baidu::ZhanZhang is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/WWW/Baidu/ZhanZhang.pm
@@ -0,0 +1,39 @@
+package WWW::Baidu::ZhanZhang;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::Baidu::ZhanZhang - Baidu ZhanZhang push links
+
+=head1 SYNOPSIS
+
+  use WWW::Baidu::ZhanZhang;
+
+=head1 DESCRIPTION
+
+WWW::Baidu::ZhanZhang is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
